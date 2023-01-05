3d6b6c940dbf65ab46ce641b73849e117e2ac4b8
gavuolo <gabs.vuolo@gmail.com>
2023-01-05 16:44:10 -0400

    Feat: post sign-up

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let tweets = [];
+let users = [];
+
+//POST Sign-up
+app.post('/signup', (req, res) => {
+    let newUser = {
+        username: req.body.username,
+        avatar: req.body.avatar
+    }
+    users.push(newUser);
+    console.log(users)
+    res.send('Ok')
+})
+
+//POST Tweets
+
+//GET Tweets
+
 //Porta
-app.listen(5000, () => {
-    console.log('Server running in port: 5000')
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Server running in port: ${PORT}`)
 })
